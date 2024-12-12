0ca0f1732dbdcf1d0316abb8a0cd04292340e406
fogel.bence <fogel.bence@hallgato.itk.ppke.hugit config --global --listlsgit remote addgit config --global user.email fogel.bence@hallgato.itk.ppke.hu>
2024-12-12 08:52:06 +0100

    Rework intrinsic current recording. Add intrinsic current saving.

--- simulation.py
+++ simulation.py
@@ -4,6 +4,8 @@ import neuron
 import numpy as np
 import time
 
+from utils.record_intrinsic import measure_intrinsic, record_intrinsic, save_intrinsic
+
 
 def simulate(model, tstop=100):
     h.CVode().active(True)
@@ -15,78 +17,11 @@ def simulate(model, tstop=100):
 
     allsec = h.allsec()
 
-    nax_vectors = []
-    nad_vectors = []
-    car_vectors = []
-    kdr_vectors = []
-    kap_vectors = []
-    kad_vectors = []
-    kslow_vectors = []
-    passive_vectors = []
-    capacitive_vectors = []
-    segments = []  # To track segments
-
-    for sec in h.allsec():
-        for seg in sec.allseg():
-            segments.append(seg)
-
-            # Na_ax
-            if hasattr(seg, '_ref_ina_nax'):
-                vec_nax = h.Vector()
-                vec_nax.record(seg._ref_ina_nax)
-                nax_vectors.append(vec_nax)
-
-            # Na_d
-            if hasattr(seg, '_ref_ina_nad'):
-                vec_nad = h.Vector()
-                vec_nad.record(seg._ref_ina_nad)
-                nad_vectors.append(vec_nad)
-
-            # Ca_R
-            if hasattr(seg, '_ref_ica_car'):
-                vec_car = h.Vector()
-                vec_car.record(seg._ref_ica_car)
-                car_vectors.append(vec_car)
-
-            # K_dr
-            if hasattr(seg, '_ref_ik_kdr'):
-                vec_kdr = h.Vector()
-                vec_kdr.record(seg._ref_ik_kdr)
-                kdr_vectors.append(vec_kdr)
-
-            # K_ap
-            if hasattr(seg, '_ref_ik_kap'):
-                vec_kap = h.Vector()
-                vec_kap.record(seg._ref_ik_kap)
-                kap_vectors.append(vec_kap)
-
-            # K_ad
-            if hasattr(seg, '_ref_ik_kad'):
-                vec_kad = h.Vector()
-                vec_kad.record(seg._ref_ik_kad)
-                kad_vectors.append(vec_kad)
-
-            # K_slow
-            if hasattr(seg, '_ref_ik_kslow'):
-                vec_kslow = h.Vector()
-                vec_kslow.record(seg._ref_ik_kslow)
-                kslow_vectors.append(vec_kslow)
-
-            # passive
-            if hasattr(seg, '_ref_i_pas'):
-                vec_ipas = h.Vector()
-                vec_ipas.record(seg._ref_i_pas)
-                passive_vectors.append(vec_ipas)
-
-            # capacitive
-            if hasattr(seg, '_ref_i_cap'):
-                vec_icap = h.Vector()
-                vec_icap.record(seg._ref_i_cap)
-                capacitive_vectors.append(vec_icap)
+    intrinsic_segments, intrinsic_currents = record_intrinsic()
 
     h.celsius = 35
     h.finitialize(-68.3)
 
     neuron.run(tstop)
 
-    inax = np.array(nax_vectors)
+    save_intrinsic(intrinsic_segments, intrinsic_currents, 'L:/test')
---
+++ utils/record_intrinsic.py
@@ -0,0 +1,64 @@
+import os
+import pandas as pd
+import numpy as np
+from neuron import h
+
+
+current_types = {
+        "nax": "_ref_ina_nax",
+        "nad": "_ref_ina_nad",
+        "car": "_ref_ica_car",
+        "kdr": "_ref_ik_kdr",
+        "kap": "_ref_ik_kap",
+        "kad": "_ref_ik_kad",
+        "kslow": "_ref_ik_kslow",
+        "passive": "_ref_i_pas",
+        "capacitive": "_ref_i_cap",
+    }
+
+
+def measure_intrinsic(seg, current_types):
+    recorded_vectors = {}
+    for current, ref_attr in current_types.items():
+        if hasattr(seg, ref_attr):
+            vec = h.Vector()
+            vec.record(getattr(seg, ref_attr))
+            recorded_vectors[current] = vec
+    return recorded_vectors
+
+
+def record_intrinsic():
+    intrinsic_currents = {current: [] for current in current_types}
+    intrinsic_segments = {current: [] for current in current_types}
+
+    for sec in h.allsec():  # Loop through all sections
+        for seg in sec.allseg():  # Loop through all segments
+            # Record currents for the segment
+            recorded = measure_intrinsic(seg, current_types)
+            for current, vec in recorded.items():
+                intrinsic_currents[current].append(vec)  # Save the recorded vector
+                intrinsic_segments[current].append(seg)  # Save the segment
+    return intrinsic_segments, intrinsic_currents
+
+
+def save_intrinsic(intrinsic_segments, intrinsic_currents, output_dir):
+    # Create output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+
+    segments_dir = os.path.join(output_dir, "intrinsic_segments")
+    currents_dir = os.path.join(output_dir, "intrinsic_currents")
+
+    os.makedirs(segments_dir, exist_ok=True)
+    os.makedirs(currents_dir, exist_ok=True)
+
+    for current_type in intrinsic_segments.keys():
+
+        segments_array = np.array(intrinsic_segments[current_type]).astype('str')
+        segments_npy_path = os.path.join(segments_dir, f"{current_type}_segments.npy")
+        np.save(segments_npy_path, segments_array)
+
+        currents_array = np.array(intrinsic_currents[current_type])
+        currents_npy_path = os.path.join(currents_dir, f"{current_type}_currents.npy")
+        np.save(currents_npy_path, currents_array)
+
+    print(f"Intrinsic data saved in directory: {output_dir}")
